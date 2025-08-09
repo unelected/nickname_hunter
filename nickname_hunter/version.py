6cedb9097e1dad4ad5fac1d9b11208017fc80f32
unelected <152802875+unelected@users.noreply.github.com>
2025-08-09 06:37:47 +0500

    Update version.py
    added docstring

--- nickname_hunter/version.py
+++ nickname_hunter/version.py
@@ -1,1 +1,7 @@
+# Copyright (c) 2024 ZS
+# Copyright (c) 2025 unelected
+#
+# This file is part of the project and is licensed under the MIT License.
+# See the LICENSE file in the root of this repository for full license text.
+
 __version__ = "2.1.1"
